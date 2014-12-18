a23a94fa8e9306e5a3751c14abc4d3cdaa728b46
JXie <JXie@SD-JXIE-LW7.na.audatex.com>
2014-12-18 13:53:52 -0800

    Add new method for find Min.

--- MyTest/src/com/jing/xie/leetcode/array/Solution.java
+++ MyTest/src/com/jing/xie/leetcode/array/Solution.java
@@ -1,5 +1,22 @@
 package com.jing.xie.leetcode.array;
 
 public class Solution {
-
+  public static int findMin(int[] num) {
+    if (num == null || num.length == 0) {
+      return 0;
+    }
+    int L = 0;
+    int R = num.length - 1;
+    while (L < R && num[L] >= num[R]) {
+      int mid = (L + R) >>> 1;
+      if (num[mid] > num[R]) {
+        L = mid + 1;
+      } else if (num[mid] < num[L]) {
+        R = mid;
+      } else {
+        L++;
+      }
+    }
+    return num[L];
+  }
 }
