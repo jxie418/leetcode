d41809fa449a11e3dc3c5afb4606f5bfb78376ec
jxie <jxie@SD-JXIE-LW7.na.audatex.com>
2015-01-23 14:52:58 -0800

    new

---
+++ NewRestWebService/src/com/jing/xie/config/core/WebAppInitializer.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.jing.xie.config.core;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.jing.xie.config.AppConfig;
+
+/**
+ * @author jxie
+ *
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    
+    AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+    ctx.register(AppConfig.class);  
+    ctx.setServletContext(servletContext);    
+    Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
+    dynamic.addMapping("/");  
+    dynamic.setLoadOnStartup(1);  
+  }
+
+}
