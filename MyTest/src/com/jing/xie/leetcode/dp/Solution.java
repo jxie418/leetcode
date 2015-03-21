007979e497b37368c5a1d8bdb3fec2344327d47b
James Xie <james.xie@audaexplore.com>
2015-03-21 14:50:41 -0700

    add new

--- MyTest/src/com/jing/xie/leetcode/dp/Solution.java
+++ MyTest/src/com/jing/xie/leetcode/dp/Solution.java
@@ -662,4 +662,26 @@ public class Solution {
     return max;
   }
   
+  public int kSum(int A[], int k, int target) {
+    if (A.length < k)
+      return 0;
+    int[][][] d = new int[k + 1][A.length + 1][target + 1];
+    for (int i = 1; i <= A.length; i++)
+      if (A[i - 1] <= target) {
+        for (int j = i; j <= A.length; j++)
+          d[1][j][A[i - 1]] = 1;
+      }
+
+    for (int i = 2; i <= k; i++)
+      for (int j = i; j <= A.length; j++)
+        for (int v = 1; v <= target; v++) {
+          d[i][j][v] = 0;
+          if (j > i)
+            d[i][j][v] += d[i][j - 1][v];
+          if (v >= A[j - 1])
+            d[i][j][v] += d[i - 1][j - 1][v - A[j - 1]];
+        }
+    return d[k][A.length][target];
+  }
+
 }
--- MyTest/src/com/jing/xie/leetcode/math/Solution.java
+++ MyTest/src/com/jing/xie/leetcode/math/Solution.java
@@ -15,4 +15,24 @@ public class Solution {
     }
     return res;
   }
+
+  public long kthPrimeNumber(int k) {
+    if (k == 0)
+      return 1;
+    long[] res = new long[k + 1];
+    res[0] = 1;
+    int p3 = 0, p5 = 0, p7 = 0;
+    for (int i = 1; i <= k; i++) {
+      // find the minimum prime number.
+      long val = Math.min(Math.min(res[p3] * 3, res[p5] * 5), res[p7] * 7);
+      if (val / res[p3] == 3)
+        p3++;
+      if (val / res[p5] == 5)
+        p5++;
+      if (val / res[p7] == 7)
+        p7++;
+      res[i] = val;
+    }
+    return res[k];
+  }
 }
