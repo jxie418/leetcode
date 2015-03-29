02535ab8bcf489511cec7bee27c72907ddad7dab
James Xie <james.xie@audaexplore.com>
2015-03-29 09:39:45 -0700

    3_29

--- HibernateExample/src/main/java/com/mastertheboss/App.java
+++ HibernateExample/src/main/java/com/mastertheboss/App.java
@@ -1,13 +1,41 @@
 package com.mastertheboss;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.Session;
+
+import com.mastertheboss.domain.Category;
+import com.mastertheboss.domain.Stock;
+import com.mastertheboss.util.HibernateUtil;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hibernate many to many (Annotation)");
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    session.beginTransaction();
+
+    Stock stock = new Stock();
+    stock.setStockCode("7052");
+    stock.setStockName("PADINI");
+
+    Category category1 = new Category("CONSUMER", "CONSUMER COMPANY");
+    Category category2 = new Category("INVESTMENT", "INVESTMENT COMPANY");
+
+    Set<Category> categories = new HashSet<Category>();
+    categories.add(category1);
+    categories.add(category2);
+
+    stock.setCategories(categories);
+
+    session.save(stock);
+
+    session.getTransaction().commit();
+    System.out.println("Done");
+  }
 }
